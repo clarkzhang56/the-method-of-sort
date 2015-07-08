fe505aeea8f55ef959475c34032de15d1769ed0c
clark <clarkzhang56@163.com>
2015-07-08 22:26:43 +0800

    Update merge_sort.c

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,3 +1,7 @@
+/*
+Merge Sort is used to sort two sorted sequence. Its biggest feature is it's a stable method.
+a and b below are the two sequence. c is the result. m and n are the length of a and b.
+*/
 void merge_sort(int *c, int *a, int *b, int m, int n){
 	int i=0, j=0, k=0;
 	while(i<m+n){
